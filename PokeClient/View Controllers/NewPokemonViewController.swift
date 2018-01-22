ec8bb6871a0072a9891abbe1461320b6674d82a2
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-22 12:01:39 +0000

    New Pokemon VC placeholder IBActions.

--- PokeClient/View Controllers/NewPokemonViewController.swift
+++ PokeClient/View Controllers/NewPokemonViewController.swift
@@ -12,11 +12,19 @@ class NewPokemonViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
     }
     
     override var preferredStatusBarStyle: UIStatusBarStyle {
         return .lightContent
+    }
+}
+
+extension NewPokemonViewController {
+    @IBAction func cancelButtonTapped(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func okButtonTapped(_ sender: Any) {
+        print("Ok button was tapped.")
     }
 }
