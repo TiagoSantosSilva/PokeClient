5e5f793e9f1fbace758cc8866795c10c4a411f69
Tiago Santos <tiagossilva_95@hotmail.com>
2018-01-31 00:27:14 +0000

    The app is now able to be opened from the browser by typing 'pokeclient://'

--- PokeClient/Application Delegate/AppDelegate.swift
+++ PokeClient/Application Delegate/AppDelegate.swift
@@ -19,6 +19,12 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         self.window?.rootViewController = loadingScreenViewController        
         return true
     }
+    
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
+//        let url = url.standardized
+//        let query = url.query
+        return true
+    }
 
     func applicationWillResignActive(_ application: UIApplication) {
         // Sent when the application is about to move from active to inactive state. This can occur for certain types of temporary interruptions (such as an incoming phone call or SMS message) or when the user quits the application and it begins the transition to the background state.
